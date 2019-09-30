5cf49606381c89b11aceb03a6d663537cc3755dc
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-30 01:41:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,15 +27,16 @@ def select_name_and_series_subgenres_of_authors
       ON series.subgenre_id = subgenres.id;"
 end
 
-def select_series_title_with_most_human_character
-    "SELECT series.title AS title, COUNT(characters.series_id) AS num_species
+def select_series_title_with_most_human_characters
+  "SELECT title FROM (
+    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
       FROM series
       INNER JOIN characters
         ON series.id = characters.series_id
       WHERE characters.species = 'human'
       GROUP BY characters.series_id
       ORDER BY num_species DESC
-      LIMIT 1;"
+      LIMIT 1);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
